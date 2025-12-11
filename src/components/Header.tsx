2cbd2eb4477965f9912c74345b12f5b5ef372e48
duongviethuy <84081789+duongviethuy@users.noreply.github.com>
2025-12-11 13:50:35 +0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,17 @@
 "use client";
+import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="flex items-center justify-around min-h-20 sticky top-0 bg-blue-300">
+        <header className="flex items-center justify-around min-h-20 sticky top-0 bg-blue-700">
             <div>
                 <h1 className="text-white font-extrabold text-5xl text-shadow-2xs">
-                    SIMPLE SHOPPING APP
+                    <Link href={"/"}>SIMPLE SHOPPING APP</Link>
                 </h1>
             </div>
-            <button>CART (0)</button>
+            <button className="bg-white text-blue-400 p-2 rounded-lg">
+                CART (0)
+            </button>
         </header>
     );
 }
