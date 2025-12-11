0756598481a4d6236ecb9576ff8c8b47cec80b6d
duongviethuy <84081789+duongviethuy@users.noreply.github.com>
2025-12-11 13:24:34 +0700

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,4 +3,10 @@
 import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 
-export default function Home() {}
+export default function Home() {
+    return (
+        <div>
+            <h1>hel</h1>
+        </div>
+    );
+}
--- src/app/types/custom.d.tsx
+++ src/app/types/custom.d.tsx
@@ -1,4 +1,1 @@
-declare module "*.css" {
-    // Chúng ta không cần gán giá trị export cụ thể cho side-effect import.
-    // Chỉ cần khai báo module tồn tại.
-}
+declare module "*.css" {}
